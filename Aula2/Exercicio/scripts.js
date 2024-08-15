eaa344d0050477362cf29912f5a9ddc366965833
Djonatan Weber <djonatan11021996@gmail.com>
2024-08-14 22:49:21 -0300

    feat: new repositorie

---
+++ Aula2/Exercicio/scripts.js
@@ -0,0 +1,46 @@
+let peso1 = 2
+let peso2 = 3
+let peso3 = 5
+
+let nota1 = 7
+let nota2 = 7
+let nota3 = 7 
+
+let mediaPonderada = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3)/(peso1 + peso2 + peso3)
+
+if (mediaPonderada >= 7) {
+  console.log("O aluno foi aprovado! Sua média foi " + mediaPonderada)
+} else { 
+  console.log("O aluno foi reprovado. Sua média foi " + mediaPonderada)
+} 
+
+
+let dia = 11
+let mes = 02
+let ano = 1996
+
+let totalDiasIdade = (ano * 365) + (mes * 30) + dia
+
+let diasAtual = 14
+let mesAtual = 08
+let anosAtual = 2024
+
+let totalDiasAtuais = (anosAtual * 365) + (mesAtual * 30) + diasAtual
+
+let idadeDias = totalDiasAtuais - totalDiasIdade
+
+console.log('A idade é ' + idadeDias + ' dias.')
+
+let num1 = 5
+let num2 = 3
+
+let soma = num1 + num2
+
+let subtracao = num1 - num2
+
+let multiplicacao = num1 * num2
+ 
+let divisao = num1 / num2
+ 
+console.log('Soma:'+ soma + ' Subtração:' + subtracao +
+            ' Multiplicação:' + multiplicacao + ' Divisão:' + divisao )
---
+++ Aula2/scripts.js
@@ -0,0 +1,130 @@
+// alert('Helllo world')
+let nome = "Djonatan"
+let nome1 = 'Carla'
+
+
+
+console.log(nome)
+console.log('Hello World!')
+
+let idade = 28
+
+console.log(idade)
+
+let preco = 19.99 
+
+console.log(preco)
+
+let estaChovendo = true
+
+console.log(estaChovendo)
+
+let sobrenome
+
+console.log(sobrenome)
+
+
+let a = 2
+let b = a
+a = 5
+
+console.log(b)
+console.log(a)
+
+let lista = 'banana, uva, morango'
+
+console.log(typeof lista)
+
+let soma = 3 + 5;
+console.log(soma)
+
+let subtracao = 7 - 2
+console.log(subtracao)
+
+let multiplicacao = 4 * 6
+console.log(multiplicacao)
+
+
+let divisao = 10 / 2
+console.log(divisao)
+
+// Resto de divsão
+let resto = 10 % 3
+console.log(resto)
+
+//Incremento
+let numero = 5
+numero++
+console.log(++numero)
+
+
+// Decremento
+let decremento = 8
+console.log(--decremento)
+
+// Exponenciação
+let potencia = 2 ** 3
+console.log(potencia)
+
+console.log(Math.pow(2,4))
+
+let num1 = 10
+let num2 = '20'
+console.log(num1+num2)
+console.log(num1+Number(num2))
+
+
+let c = 10
+let d = 3
+
+c +=5
+
+console.log(c)
+
+console.log(d-= 1)
+
+c *= 3
+console.log(c)
+
+d /= 4
+console.log(d)
+
+c %= 5
+console.log(c) 
+
+let num3 = 5
+let num4 = 7
+
+let maior = num3 > num4
+console.log(maior)
+
+let menor = num3 < num4
+console.log(menor)
+
+let maiorOuIgual = num3 >= num4
+console.log(maiorOuIgual)
+
+let menorOuIgual = num3 <= num4
+console.log(menorOuIgual)
+
+let igual = num3 == '5'
+console.log(num3)
+console.log(igual)
+
+let diferente = num3 != 5
+console.log(diferente)
+
+let haveMoney = false
+if (haveMoney) {
+  console.log('Vou passear no domingo.')
+} else {
+  console.log('Não vou passear no domingo.')
+}
+
+let num = 10
+if (num >= 10){
+  console.log('Sim! O número é maior que a 10')
+} else {
+  console.log('Não. O número é maior que a 10')
+}
+

d6efed19c1a2c6b01f8520c4f19926b3401b8e3f
Djonatan Weber <djonatan11021996@gmail.com>
2024-09-09 21:51:18 -0300

    feat: Loops repet.

--- Aula-9/scripts.js
+++ Aula-9/scripts.js
@@ -1,3 +1,4 @@
+/*
 const idade = 16
 
 if (idade < 13) {
@@ -25,3 +26,54 @@ const idade2 = 20
 const status = (idade2 >= 18) ? 'Adulto' : 'Menor'
 
 console.log(status)
+
+const numeroAleatorio = Math.floor(Math.random() * 10) + 1
+let tentativas = 0
+let acertou = false
+
+while(!acertou) {
+  const numero = parseInt(prompt('Digite um número'))
+  tentativas ++
+  if (numero === numeroAleatorio) {
+    alert(`Parabéns você acertou em ${tentativas} tentativas`)
+    acertou = true
+  } else {
+    alert('Tente novamente!')
+  } 
+
+}
+
+const numeroAleatorio2 = Math.floor(Math.random() * 10) + 1
+let tentativas2 = 0
+let acertou2 = false
+
+do {
+  const numero = parseInt(prompt('Digite um número'))
+  tentativas ++
+  if (numero === numeroAleatorio2) {
+    alert(`Parabéns você acertou em ${tentativas2} tentativas`)
+    acertou2 = true
+  } else {
+    alert('Tente novamente!')
+  } 
+}while(!acertou2) {
+}
+
+for (let tentativas = 0; !acertou; tentativas++) {
+
+}
+*/
+const frutas = ['Maça', 'Banana', 'Uva']
+for (const fruta of frutas) {
+  console.log(fruta)
+}
+
+const livro = {
+  titulo: 'A única coisa',
+  numeroPaginas: 250,
+  idioma: 'english'
+}
+
+for (const chave in livro) {
+  console.log(livro[chave])
+}

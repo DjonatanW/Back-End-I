124b841798ada2f6414c7794d834c380da48302e
Djonatan Weber <djonatan11021996@gmail.com>
2024-09-11 22:22:49 -0300

    feat: Activity revision.

--- Aula-10/Exercies-Aula-10/scripts.js
+++ Aula-10/Exercies-Aula-10/scripts.js
@@ -9,3 +9,40 @@ console.log(newlistArray.filter(valor => valor >= 12))
 newlistArray.push(1)
 console.log(newlistArray)
 console.log(newlistArray.findIndex(valor => valor % 2 != 0))
+
+const promocoesEspeciais = []
+
+promocoesEspeciais.push({
+  id: 356,
+  productName: 'wwrgvrg',
+  productValue: 30,
+  stockQuantity:220
+})
+
+promocoesEspeciais.push({
+  id: 3,
+  productName: 'wrg',
+  productValue: 20,
+  stockQuantity:220
+})
+promocoesEspeciais.push({
+  id: 321,
+  productName: 'wrgsfbgv',
+  productValue: 200,
+  stockQuantity:220
+})
+console.log(promocoesEspeciais);
+console.log(promocoesEspeciais.filter(objeto => objeto.id === 3))
+
+const objetoPosicao = promocoesEspeciais.filter(objeto => objeto.id === 3)
+for (const posicao in promocoesEspeciais) {
+  if (promocoesEspeciais[posicao].id === 3) {
+    promocoesEspeciais[posicao].productName = 'Teste'
+  }
+}
+console.log(promocoesEspeciais)
+promocoesEspeciais.splice(promocoesEspeciais.findIndex(objeto => objeto.id === 3), 1)
+console.log("Produto deletado com sucesso !")
+console.log(promocoesEspeciais)
+
+

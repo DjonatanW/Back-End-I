c007b4dd448881b46128cae4d8a001c04a380de3
Djonatan Weber <djonatan11021996@gmail.com>
2024-09-19 22:22:33 -0300

    feat: revision node and api.

---
+++ Revision-Node/src/potencia.js
@@ -0,0 +1,7 @@
+// let x = parseInt(prompt('Digite o primeiro número'))
+// let y = parseInt(prompt('Digite o segundo número'))
+// let z = parseInt(prompt('Digite o terceiro número'))
+let x = 2
+let y = 4
+let z = 20
+console.log(`A potência de número X é ${ x**2 },  Y é ${ y**2 } e Z é ${ z**2 } `);

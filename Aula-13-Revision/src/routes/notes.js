82f4babaa1d22d7442e6ec06af99539acf2d4079
Djonatan Weber <djonatan11021996@gmail.com>
2024-10-02 19:11:31 -0300

    feat: create router post.

--- Aula-13-Revision/src/routes/notes.js
+++ Aula-13-Revision/src/routes/notes.js
@@ -6,4 +6,32 @@ const router = express.Router()
 
 const notes = []
 
+router.post('/', (request, response) => {
+  const { title, description, userId } = request.body
+
+  const user = users.find(user => user.id === userId)
+
+  if (!user) {
+    return response.status(404).json({
+      message: 'Usuário não encontado.'
+    })
+  }
+
+  const newNote = {
+    id: uuidv4(),
+    title,
+    description,
+    userId
+  }
+
+  notes.push(newNote)
+
+  response.status(201).json({
+    message: 'Recado cadastrado com sucesso.',
+    note: newNote
+  })
+})
+
+
+
 export default router
